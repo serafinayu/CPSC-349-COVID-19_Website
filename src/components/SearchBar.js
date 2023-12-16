47d3288173a32f932f29d3b82ca889e87013e117
NoName <daoducthienan007@gmail.com>
2023-12-15 16:30:49 -0800

    Create SearchBar.js

---
+++ src/components/SearchBar.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+const SearchBar = ({ onSearch }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearch = () => {
+    onSearch(searchTerm);
+  };
+
+  return (
+    <div className="search-bar">
+      <input
+        type="text"
+        placeholder="Enter country or area name"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <button onClick={handleSearch}>Search</button>
+    </div>
+  );
+};
+
+export default SearchBar;
